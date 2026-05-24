1e25d7086a32b14e2a9846901c2a95a6ef0df009
Axel <eriksundqvist11@gmail.com>
2026-05-24 23:50:27 +0200

    Added more xml files and tests

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -69,10 +69,12 @@ void test_xml_get_partial_key_returns_null(void) {
 void test_list_simple(void) {
     XmlMap map = {0};
     TEST_ASSERT_EQUAL(XML_OK, parse("../configs/list.xml", &map));
-    TEST_ASSERT_EQUAL(3, map.count);
-    TEST_ASSERT_EQUAL_STRING("apple",  xml_get(&map, "list.item[0]"));
-    TEST_ASSERT_EQUAL_STRING("banana", xml_get(&map, "list.item[1]"));
-    TEST_ASSERT_EQUAL_STRING("cherry", xml_get(&map, "list.item[2]"));
+    TEST_ASSERT_EQUAL(5, map.count);
+    TEST_ASSERT_EQUAL_STRING("apple",      xml_get(&map, "shopping.item[0]"));
+    TEST_ASSERT_EQUAL_STRING("banana",     xml_get(&map, "shopping.item[1]"));
+    TEST_ASSERT_EQUAL_STRING("cherry",     xml_get(&map, "shopping.item[2]"));
+    TEST_ASSERT_EQUAL_STRING("dates",      xml_get(&map, "shopping.item[3]"));
+    TEST_ASSERT_EQUAL_STRING("elderberry", xml_get(&map, "shopping.item[4]"));
 }
 
 void test_list_objects(void) {
@@ -81,9 +83,30 @@ void test_list_objects(void) {
     TEST_ASSERT_EQUAL_STRING("30",                xml_get(&map, "users.user[0].@age"));
     TEST_ASSERT_EQUAL_STRING("Alice",             xml_get(&map, "users.user[0].name"));
     TEST_ASSERT_EQUAL_STRING("alice@example.com", xml_get(&map, "users.user[0].email"));
+    TEST_ASSERT_EQUAL_STRING("admin",             xml_get(&map, "users.user[0].role"));
     TEST_ASSERT_EQUAL_STRING("20",                xml_get(&map, "users.user[1].@age"));
     TEST_ASSERT_EQUAL_STRING("Axel",              xml_get(&map, "users.user[1].name"));
     TEST_ASSERT_EQUAL_STRING("axel@example.com",  xml_get(&map, "users.user[1].email"));
+    TEST_ASSERT_EQUAL_STRING("developer",         xml_get(&map, "users.user[1].role"));
+    TEST_ASSERT_EQUAL_STRING("25",                xml_get(&map, "users.user[2].@age"));
+    TEST_ASSERT_EQUAL_STRING("Bob",               xml_get(&map, "users.user[2].name"));
+    TEST_ASSERT_EQUAL_STRING("bob@example.com",   xml_get(&map, "users.user[2].email"));
+    TEST_ASSERT_EQUAL_STRING("viewer",            xml_get(&map, "users.user[2].role"));
+}
+
+void test_bad_unclosed(void) {
+    XmlMap map = {0};
+    TEST_ASSERT_EQUAL(XML_ERR_PARSE, parse("../configs/bad_unclosed.xml", &map));
+}
+
+void test_bad_mismatch(void) {
+    XmlMap map = {0};
+    TEST_ASSERT_EQUAL(XML_ERR_PARSE, parse("../configs/bad_mismatch.xml", &map));
+}
+
+void test_bad_amp(void) {
+    XmlMap map = {0};
+    TEST_ASSERT_EQUAL(XML_ERR_PARSE, parse("../configs/bad_amp.xml", &map));
 }
 
 void test_overflow(void) {
@@ -109,5 +132,8 @@ int main(void) {
     RUN_TEST(test_list_simple);
     RUN_TEST(test_list_objects);
     RUN_TEST(test_overflow);
+    RUN_TEST(test_bad_unclosed);
+    RUN_TEST(test_bad_mismatch);
+    RUN_TEST(test_bad_amp);
     return UNITY_END();
 }
